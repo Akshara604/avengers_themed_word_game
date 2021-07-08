fe6d15fadce9ecb3ef8cfae08e83b77db346fa92
Akshara S PA <74604911+Akshara604@users.noreply.github.com>
2021-07-08 21:29:44 +0530

    Update avengersGame.py

--- Avengers game/avengersGame.py
+++ Avengers game/avengersGame.py
@@ -2,7 +2,7 @@ from loki import *
 from spiderman import *
 print("Hey there! Yeah you!")
 print("Let\'s play a game")
-print("You will have 50 points.Each time you loose, 10 points will be reduced.Each time you win, 10 will be added.")
+print("You will have 50 points.Each time you lose, 10 points will be reduced.Each time you win, 10 will be added.")
 print("If you reach 100 points you win. If your points get reduced to 0 you loose.")
 print("So, make good choices!")
 print("Choose a character from the list below:")
